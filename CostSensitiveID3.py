879196312b658e5cca652a1dbd4b42eee4a04c4e
tomCohen123 <tmk@campus.technion.ac.il>
2021-01-18 23:06:39 +0200

    get answer from teacher, finish cost sensitive ID3 make forest class to save in it parameters to do experiments

--- CostSensitiveID3.py
+++ CostSensitiveID3.py
@@ -3,9 +3,35 @@ import ID3 as id_three
 # todo: what experiments?
 # todo: train data preprocessing?
 # todo: change v group to be part of train
+# todo: use a change of entropy?
 
-id3 = id_three.ID3(is_early_pruning=False, limit=None, predict_dict=id_three.test_group_dict)
+#id3 = id_three.ID3(is_early_pruning=False, limit=None, predict_dict=id_three.test_group_dict)
 
+class CostSensitiveID3(id_three.ID3):
+    def __init__(self,  is_early_pruning, limit, predict_dict, ):
+        id_three.ID3.__init__(self,limit=limit, is_early_pruning= is_early_pruning, predict_dict= predict_dict)
+
+    def latePruning(self, node, v):
+        if node.l_son is None and node.r_son is None:
+            return node
+
+        smaller_v, bigger_equale_v = id_three.splitIndexs(f=node.feature, barrier=node.barrier, examples_indices=v)
+        node.l_son = self.latePruning(node.l_son, smaller_v)
+        node.r_son = self.latePruning(node.r_son, bigger_equale_v)
+
+        err_prune = 0
+        err_no_prune = 0
+        for idx in v:
+            v_idx_label = id_three.train_group_dict['diagnosis'][idx]
+            err_prune += evaluate(v_idx_label, node.label)
+            err_no_prune += evaluate(v_idx_label, self.classifier(idx, node))
+
+        if err_prune < err_no_prune:
+            node.f = None
+            node.l_son = None
+            node.r_son = None
+
+        return node
 
 def evaluate(v_idx_label, tree_label):
     if v_idx_label == tree_label:
@@ -13,35 +39,48 @@ def evaluate(v_idx_label, tree_label):
     return 9 if v_idx_label == 'M' else 1
 
 
-def latePruning(node, v):
-    if node.l_son==None and node.r_son==None:
-        return node
 
-    smaller_v, bigger_equale_v = id_three.splitIndexs(f=node.feature, barrier=node.barrier, examples_indices=v)
-    node.l_son = latePruning(node.l_son, smaller_v)
-    node.r_son = latePruning(node.r_son, bigger_equale_v)
 
-    err_prune = 0
-    err_no_prune = 0
-    for idx in v:
-        v_idx_label = id_three.test_group_dict['diagnosis'][idx]
-        err_prune += evaluate(v_idx_label, node.label)
-        err_no_prune += evaluate(v_idx_label, id3.classifier(idx,node))
+"""
+v must be a part of train test. Thus we need to split train into 2 groups: actual training group and validation group.
+I started from taking first 50 indices of train for validation because it seemed to me
+a logical number, and the rest to actual training.
+Then each iteration i took the 50 next indices from the actual train group and add them to the validation group,  
+until 50 indexes left in the train group. 
+"""
+def chooseVExperiment():
+    experiment_id3 = CostSensitiveID3(is_early_pruning=False, limit=None, predict_dict=id_three.train_group_dict)
+    threshold = 1
+    ###loss is between 0 to 1 thus if i intialized it to that value to be sure best_loss will be updated in first iteration.
+    best_loss = 2
+    best_threshold = 1
+    train_group_len = len(id_three.train_row_indices)
 
-    if err_prune < err_no_prune:
-        node.f = None
-        node.l_son = None
-        node.r_son = None
+    while train_group_len-threshold >= 30:
+        experiment_id3.fit(id_three.train_row_indices[threshold:])
+        experiment_id3.decision_tree = experiment_id3.latePruning(experiment_id3.decision_tree, id_three.train_row_indices[:50])
+        cur_loss = experiment_id3.predictLoss(id_three.train_row_indices[50:100])
+        if cur_loss < best_loss:
+            best_loss = cur_loss
+            best_threshold = threshold
+        threshold += 50
+    print(best_threshold)
+    print(best_loss)
 
-    return node
 
 
 def main():
     # because my optimal m was 1, it means no prune, i use the algorithem with no pruning
-    id3.fit(id_three.train_row_indices)
-    id3.decision_tree = latePruning(id3.decision_tree, id_three.test_row_indices)
+#todo: uncomment below
+    id3 = CostSensitiveID3(is_early_pruning=False, limit=None, predict_dict=id_three.train_group_dict)
+    id3.fit(id_three.train_row_indices[50:])
+    id3.decision_tree = id3.latePruning(id3.decision_tree, id_three.train_row_indices[:50])
+    id3.predict_dict=id_three.test_group_dict
     print(id3.predictLoss(id_three.test_row_indices))
+# todo: uncomment above
 
-
+# todo: comment below
+    #chooseVExperiment()
+# todo: comment above
 if __name__ == "__main__":
     main()
--- improvedKNNForest.py
+++ improvedKNNForest.py
@@ -2,3 +2,9 @@
 # use late pruning on each tree if there is enough time
 # preprocessing check if there is noise  examples
 # even if there is not tell that i did it
+#look for best group v for late pruning with barrier and random
+#choose a smaller learning group with parameter tuning to avoid overfiting
+#give different weight to features some how
+#normalization of paramerter
+#can improve loss also
+#find best train subgroup from train with random size and places
